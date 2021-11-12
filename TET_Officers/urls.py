2e87314b6a9de40ec8c26fd5c344f4c75d51b278
Rameshwar Chavan <chavanrm234@gmail.com>
2021-11-13 02:10:47 +0530

    url modified for revers route

--- TET_Officers/urls.py
+++ TET_Officers/urls.py
@@ -5,5 +5,5 @@ from django.core import serializers
 app_name = 'TET_Officers'
 urlpatterns = [
     # path("", views.index, name="index"),
-    path("TET_Officers/admin_home", views.admin_index, name="admin_index"),
+    path("TET_Officers/admin_index", views.admin_index, name="admin_index"),
 ]
