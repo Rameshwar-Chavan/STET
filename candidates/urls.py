a56df928e4e946add8a498781561f5379a6cbf00
Mangesh <mshirodkar53@gmail.com>
2021-11-12 13:09:01 +0530

    Modified URLS

--- candidates/urls.py
+++ candidates/urls.py
@@ -6,5 +6,6 @@ from django.core import serializers
 app_name = 'candidates'
 urlpatterns = [
     path("Applicant_Registration/", views.registration, name="registration"),
-    path("Applicant_Login/", views.login, name="login")
+    path("Applicant_Login/", views.login, name="login"),
+    path("Applicant_Dashboard/", views.dashboard, name="dashboard")
 ]
