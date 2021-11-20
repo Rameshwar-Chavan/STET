be2168628a5bb345dd68bf2cf5abef91c8cb436c
Mangesh <mshirodkar53@gmail.com>
2021-11-20 12:32:40 +0530

    Added Models

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class applicant_registration(models.Model):
+
+    applicant_first_name = models.CharField(max_length=20)
+    applicant_last_name = models.CharField(max_length=30)
+    email_id = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    mobile = models.BigIntegerField()
+
+    def __str__(self):
+        return self.applicant_first_name
