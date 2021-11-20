7b8a422f410229d6524efca8034666e2251b6213
white_devil <dipaks3344@gmail.com>
2021-11-20 10:57:52 +0530

    changes added in admin index file

--- TET_Officers/views.py
+++ TET_Officers/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def admin_index(request):
     return render(request, 'admin_home/admin_index.html',{})
+def registration(request):
+    return render(request, 'admin_home/Admin_Registration.html',{})
+def login(request):
+    return render(request, 'admin_home/Admin_Login.html',{})
