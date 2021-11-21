ef14d34628668e6500eb01aa83a7e703e83429d9
Rameshwar Chavan <chavanrm234@gmail.com>
2021-11-21 13:29:12 +0530

    modified views

--- TET_Officers/views.py
+++ TET_Officers/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def admin_index(request):
-    return render(request, 'admin_home/admin_index.html',{})
-def registration(request):
-    return render(request, 'admin_home/Admin_Registration.html',{})
-def login(request):
-    return render(request, 'admin_home/Admin_Login.html',{})
+
+
+def officer_home(request):
+    return render(request, 'admin_home/admin_index.html', {})
+
+
+def Exam_Officer_Login(request):
+    return render(request, 'admin_home/Exam_Officer_Login.html', {})
+
+
+def Exam_Investigator_Login(request):
+    return render(request, 'admin_home/Exam_Investigator_Login.html', {})
