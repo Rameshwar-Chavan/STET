879e14104fdf97d01e725983e7ea2b6e1397fad4
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-01 14:52:35 +0530

    added new views

--- TET_Officers/views.py
+++ TET_Officers/views.py
@@ -4,12 +4,24 @@ from django.shortcuts import render
 
 
 def officer_home(request):
-    return render(request, 'admin_home/admin_index.html', {})
+    return render(request, 'admin_home/index_admin.html', {})
 
 
-def Exam_Officer_Login(request):
+def exam_officer_login(request):
     return render(request, 'admin_home/Exam_Officer_Login.html', {})
 
 
-def Exam_Investigator_Login(request):
+def exam_investigator_login(request):
     return render(request, 'admin_home/Exam_Investigator_Login.html', {})
+
+
+def notification(request):
+    return render(request, 'admin_home/notifications.html', {})
+
+
+def timetable(request):
+    return render(request, 'admin_home/timetable.html', {})
+
+
+def results(request):
+    return render(request, 'admin_home/result.html')
