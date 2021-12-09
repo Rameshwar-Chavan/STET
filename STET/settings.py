a69e85be3597de5e36ba579d87c9ef3ef567bd39
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-09 17:13:19 +0530

    Modified

--- STET/settings.py
+++ STET/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #My Apps
+    # My Apps
     'candidates.apps.CandidatesConfig',
     'TET_Officers.apps.TetOfficersConfig',
 ]
