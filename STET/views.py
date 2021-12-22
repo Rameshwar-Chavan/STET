63290ddbba8c3920f8af781a459db4095f1cb7e8
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-22 19:01:43 +0530

    Modified

--- STET/views.py
+++ STET/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from TET_Officers.models import Notifications
+
+
+def master(request):
+    all_notifications = Notifications.objects.all()
+    return render(request, 'base_home.html', {'all_notifications': all_notifications})
+
 
 def index(request):
-    return render(request, 'index.html',{})
+    all_notifications = Notifications.objects.all()
+    return render(request, 'index.html', {'all_notifications': all_notifications})
+
 
 def about(request):
-    return render(request,'about.html',{})
+    all_notifications = Notifications.objects.all()
+    return render(request, 'about.html', {'all_notifications': all_notifications})
+
 
 def contact(request):
-    return render(request,'contact.html',{})
+    all_notifications = Notifications.objects.all()
+    return render(request, 'contact.html', {'all_notifications': all_notifications})
