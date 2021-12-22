b776c53f597bef95ba7a1fca6ed979db6c72f958
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-22 19:02:27 +0530

    Modified

--- TET_Officers/admin.py
+++ TET_Officers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Notifications
 # Register your models here.
+
+
+# @admin.register(Notifications)
+# class NotificationsAdmin(admin.ModelAdmin):
+#     list_display = ("ID", "Notification Name",
+#                     "Start Date", "End Date", "Action")
