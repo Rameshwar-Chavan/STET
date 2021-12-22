a0dea7641faef5d61583a9561f227f2354239727
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-22 19:04:15 +0530

    Modified

--- candidates/models.py
+++ candidates/models.py
@@ -11,3 +11,15 @@ class applicant_registration(models.Model):
 
     def __str__(self):
         return self.applicant_first_name
+
+
+class Profile(models.Model):
+
+    candidate_first_name = models.CharField(max_length=20)
+    candidate_last_name = models.CharField(max_length=30)
+    email_id = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    mobile = models.BigIntegerField()
+
+    def __str__(self):
+        return self.applicant_first_name
