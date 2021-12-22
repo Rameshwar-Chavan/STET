e02fc2ca5854670d188db8850be45cff5517f1ae
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-22 19:03:54 +0530

    Modified

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+# from django.contrib.admin.decorators 
+from django.db import models
+from django.db.models.base import Model
 
 # Register your models here.
+from .models import applicant_registration
+
+
+# @admin.register(applicant_registration)
+# class AdminApplicants(admin.ModelAdmin):
+#     list_display = ('ID', 'User Name', 'Mobile No', 'Email Id')
