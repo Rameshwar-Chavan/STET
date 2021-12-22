60c426f226e53138ad132f3f77d2e01069da84ac
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-22 19:03:02 +0530

    Modified

--- TET_Officers/models.py
+++ TET_Officers/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Notifications(models.Model):
+
+    notification_name = models.CharField(max_length=20)
+    notification_str_date = models.DateField(
+        auto_now=False, auto_now_add=False, null=True)
+    notification_end_date = models.DateField(
+        auto_now=False, auto_now_add=False, null=True)
+
+    def __str__(self):
+        return self.id
+
+
+class TimeTable(models.Model):
+
+    exam_name = models.CharField(max_length=20)
+    exam_str_date = models.DateField(
+        auto_now=False, auto_now_add=False, null=True)
+    exam_end_date = models.DateField(
+        auto_now=False, auto_now_add=False, null=True)
+
+    def __str__(self):
+        return self.id
