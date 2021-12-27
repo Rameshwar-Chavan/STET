171862093da14a478555ca7d38f536e997b9667c
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-28 00:00:32 +0530

    New Url's fixed

--- candidates/urls.py
+++ candidates/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("Applicant_Registration/", views.registration, name="registration"),
     path("Applicant_Login/", views.login, name="login"),
     path("Applicant_Dashboard/", views.dashboard, name="dashboard"),
-    path("Applicant_profile/", views.profile, name="profile")
+    path("Applicant_profile/", views.profile, name="profile"),
+    path("All_Application/", views.all_application, name="All_Application"),
+    path("Post_Apply/", views.post_apply, name="Post_Apply"),
 ]
