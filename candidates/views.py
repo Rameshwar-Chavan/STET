cc165df6ecc76dcbbe42d071a9079f0122489060
Rameshwar Chavan <chavanrm234@gmail.com>
2021-12-28 00:00:51 +0530

    New views added

--- candidates/views.py
+++ candidates/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from candidates.models import applicant_registration
+from TET_Officers.models import Notifications
 from django.contrib import messages
 from django.http import HttpResponse
+
 
 def registration(request):
     if request.method == "POST":
@@ -13,19 +15,33 @@ def registration(request):
             insert.password = request.POST.get('password')
             insert.mobile = request.POST.get('mobile')
             insert.save()
-            messages.success(request, 'Applicant Registration Successfully Completed !')
+            messages.success(
+                request, 'Applicant Registration Successfully Completed !')
             return render(request, 'students/Applicant_Registration.html', {})
         else:
-            messages.error(request,'Applicant Is Already Registered!')
+            messages.error(request, 'Applicant Is Already Registered!')
             return render(request, 'students/Applicant_Registration.html', {})
 
     return render(request, 'students/Applicant_Registration.html', {})
 
+
 def login(request):
     return render(request, 'students/Applicant_Login.html', {})
+
 
 def dashboard(request):
     return render(request, 'students/Applicant_Dashboard.html', {})
 
+
 def profile(request):
-    return render(request, 'students/Applicant_profile.html',{})
+    return render(request, 'students/Applicant_profile.html', {})
+
+
+def post_apply(request):
+    all_notifications = Notifications.objects.all()
+    return render(request, 'admin_home/apply.html', {'all_notifications': all_notifications})
+
+
+def all_application(request):
+    all_notifications = Notifications.objects.all()
+    return render(request, 'students/all_application.html', {'all_notifications': all_notifications})
