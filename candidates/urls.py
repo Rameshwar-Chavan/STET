53a8481def4bcadcdb800334a3557d7715cf6050
Rameshwar Chavan <chavanrm234@gmail.com>
2022-01-01 12:19:23 +0530

    Modified

--- candidates/urls.py
+++ candidates/urls.py
@@ -6,7 +6,8 @@ from django.core import serializers
 app_name = 'candidates'
 urlpatterns = [
     path("Applicant_Registration/", views.registration, name="registration"),
-    path("Applicant_Login/", views.login, name="login"),
+    path("Applicant_Login/", views.user_login, name="login"),
+    path("user_logout/", views.user_logout, name="user_logout"),
     path("Applicant_Dashboard/", views.dashboard, name="dashboard"),
     path("Applicant_profile/", views.profile, name="profile"),
     path("All_Application/", views.all_application, name="All_Application"),
--- candidates/views.py
+++ candidates/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from candidates.models import applicant_registration, personal_information
 from TET_Officers.models import Notifications
 from django.contrib import messages
 from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout, validators
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from STET.decorators import unauthorized_user, allowed_users
 
 
 def registration(request):
@@ -25,14 +31,12 @@ def registration(request):
     return render(request, 'students/Applicant_Registration.html', {})
 
 
-def login(request):
-    return render(request, 'students/Applicant_Login.html', {})
-
-
+@login_required(login_url='candidates:login')
 def dashboard(request):
     return render(request, 'students/Applicant_Dashboard.html', {})
 
 
+@login_required(login_url='candidates:login')
 def profile(request):
     if request.method == "POST":
         if request.POST.get('first_name') and request.POST.get('middle_name') and request.POST.get('last_name') and request.POST.get('email') and request.POST.get('mobile') and request.POST.get('d_o_b') and request.POST.get('gender') and request.POST.get('age') and request.POST.get('marital_status'):
@@ -48,7 +52,7 @@ def profile(request):
             insert.marital_status = request.POST.get('marital_status')
             insert.save()
             messages.success(
-               request, 'Personal Information Successfully Saved!')
+                request, 'Personal Information Successfully Saved!')
             return render(request, 'students/Applicant_profile.html', {})
         else:
             messages.error(request, 'Applicant Is Already Registered!')
@@ -57,11 +61,38 @@ def profile(request):
     return render(request, 'students/Applicant_profile.html', {})
 
 
+@login_required(login_url='candidates:login')
 def post_apply(request):
     all_notifications = Notifications.objects.all()
     return render(request, 'admin_home/apply.html', {'all_notifications': all_notifications})
 
 
+@login_required(login_url='candidates:login')
 def all_application(request):
     all_notifications = Notifications.objects.all()
     return render(request, 'students/all_application.html', {'all_notifications': all_notifications})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('stud_username')
+        password = request.POST.get('stud_password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login Successfull')
+            return redirect('candidates:dashboard')
+        else:
+            messages.error(request, 'Incorrect User Name or Password')
+            return render(request, 'students/Applicant_Login.html')
+
+    context = {}
+    return render(request, 'students/Applicant_Login.html', context)
+
+
+# Logout user View
+@login_required(login_url='candidates:login')
+def user_logout(request):
+    logout(request)
+    return redirect('candidates:login')
